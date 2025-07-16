e66257039489e83c048b7ab2dad429dff89fcad8
Bucur Rares <bucur.rares04@yahoo.com>
2025-07-16 13:30:11 +0300

    Se vor creea alte fisiere cu scriptu-uri care se vor executa

--- sync_files.sh
+++ sync_files.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+#192.168.56.101 ip masina virtuala
+clear
+#separat pentru acele 10 minute sincronizate si sa fie bidirectional local -> remote si remote -> local
+script=sync.sh
+if [[ ! -f $script ]]
+then
+echo "#!/bin/bash" > $script
 
-LOCAL_DIR="/home/rares/Desktop/test"
-REMOTE_USER="rares"
-REMOTE_HOST="192.168.56.101"
-REMOTE_DIR="/home/rares/Desktop/Test"
+cat << 'EOF' >> "$script"
+
+LOCAL_DIR=$1
+REMOTE_USER=$2
+REMOTE_HOST=$3
+REMOTE_DIR=$4
 TMP_DIR=".sync_tmp"
-#de modificat sa pot mai multe fisiere o data sa le monitorizez
+
 declare -A recent_files
 
 sync_file() {
@@ -41,17 +50,15 @@ sync_file() {
     fi
 
 
-    if  scp "$file_path" "$REMOTE_USER@$REMOTE_HOST:$temp_dir"; then #termina verificare cu hash-uri
-        #local_hash=$(sha256sum "$file_path" | cut -d ' ' -f 1)
-        #remote_hash=$(ssh "$REMOTE_USER@$REMOTE_HOST" "sha256sum '$remote_path' 2>/dev/null | cut -d ' ' -f 1")
-        #if [[ "$local_hash" == "$remote_hash" ]]; then
-        #    abc
-       # fi
-         ssh "$REMOTE_USER@$REMOTE_HOST" "
+    if  scp "$file_path" "$REMOTE_USER@$REMOTE_HOST:$temp_dir"; then
+
+        ssh "$REMOTE_USER@$REMOTE_HOST" "
             while lsof '$remote_path' &>/dev/null; do sleep 1; done
-            mv '$temp_path' '$remote_path'
-            rmdir --ignore-fail-on-non-empty '$temp_dir' 2>/dev/null || true
         "
+        ssh "$REMOTE_USER@$REMOTE_HOST" "
+            mv '$temp_path' '$remote_path'
+            rmdir --ignore-fail-on-non-empty '$temp_dir' 2>/dev/null || true"
+            
         echo "[✓] Transfer reusit: $rel_path"
     else
         echo "[!] Eroare la transfer: $rel_path"
@@ -90,4 +97,83 @@ while true; do
         sync_file "$file"
     done
     cleanup_remote_files
+done
+EOF
+
+sudo chmod +x $script
+
+fi
+
+schimba_date(){
+read -p "Scrieti numele masinii remote: " remote_name
+
+read -p "Scrieti ip-ul numele masinii remote: " remote_ip
+
+until  ssh -o BatchMode=yes -o ConnectTimeout=5 "$remote_name@$remote_ip" exit 0
+do
+
+echo "Nume sau ip gresit reintroduceti"
+
+read -p "Nume:" remote_name
+
+read -p "IP:" remote_ip
+
+done
+
+echo
+}
+
+sincronizare_fis(){
+if [[ -n $remote_name || -n $remote_ip ]]
+then
+echo "Scrieti locatia fisierului local(calea completa)"
+echo "Ex:/home/user/director"
+read local
+
+while [ ! -d "$local" ] 
+do
+
+read -p "Locatia data este invalida reintroduceti: " local
+
+done
+
+echo
+
+echo "Scrieti locatia fisierului remote(calea completa)"
+echo "Ex:/home/user_remote/director"
+read remote
+
+while ssh -o BatchMode=yes -o ConnectTimeout=5 "$remote_name@$remote_ip" '[ ! -d '$remote' ]' 
+do
+
+read -p "Locatia data este invalida reintroduceti: " remote
+
+done
+
+echo
+
+
+
+gnome-terminal -- /home/rares/Desktop/"$script" $local $remote_name $remote_ip $remote 
+else
+    echo "Nu au fost selectate datele pentru conectarea la alta masina"
+fi
+}
+
+afiseaza_datele(){
+    echo "Nume remote: $remote_name"
+    echo "IP remote: $remote_ip"
+}
+
+PS3="Alege o optiune: "
+
+select meniu in "Sincronizeaza fisiere" "Schimba datele din ssh" "Afiseaza datele remote" "Exit"
+do
+    case $REPLY in 
+    1)sincronizare_fis ;;
+    2)schimba_date ;;
+    3)afiseaza_datele ;;
+    4)exit 0 ;;
+    *)echo "Optiune invalida"
+    esac
 done
